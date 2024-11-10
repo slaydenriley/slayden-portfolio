beb0870013545da8a08468e3699414a5c8c79b1e
slaydenriley <slaydenriley@gmail.com>
2024-11-09 22:14:53 -0600

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/slayden-portfolio",
   plugins: [react()],
   server: {
     open: true,
